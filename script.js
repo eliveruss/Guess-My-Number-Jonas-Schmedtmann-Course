8bc1d6329ec1b7f4b67460b77c2faf443dfe7d48
Olek <aleksander.galdyn@o2.pl>
2023-05-04 23:19:17 +0200

    add functions and logic

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(`.number`).textContent = secretNumber;
+
+const message = document.querySelector('.message');
+
+let score = 20;
+
+document.querySelector(`.check`).addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    message.textContent = 'No number';
+  } else if (guess === secretNumber) {
+    message.textContent = 'Correct number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      message.textContent = 'Too high! Try again. -1 score';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      message.textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = '0';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      message.textContent = 'Too low! Try again. -1 score';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      message.textContent = 'You lost the game!';
+    }
+  }
+});
